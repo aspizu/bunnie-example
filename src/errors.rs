d3cf7937623be42a7006ae2504dddf4f5af6bc77
aspizu <aspizu@protonmail.com>
2026-02-02 20:42:27 +0530

    refactor: change AppError to enum with Eyre and NotFound (#9)

--- src/errors.rs
+++ src/errors.rs
@@ -9,14 +9,21 @@ use axum::{
 // Use eyre, define error and enable '?'
 // For a simplified example of using eyre in axum check /examples/eyre-error-response
 #[derive(Debug)]
-pub struct AppError(eyre::Report);
+pub enum AppError {
+    Eyre(eyre::Report),
+    NotFound,
+}
 
 // Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        tracing::error!("Application error: {:#}", self.0);
-
-        (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+        match self {
+            AppError::Eyre(err) => {
+                tracing::error!("Application error: {:#}", err);
+                (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+            }
+            AppError::NotFound => (StatusCode::NOT_FOUND, "Not found").into_response(),
+        }
     }
 }
 
@@ -26,6 +33,6 @@ impl<E> From<E> for AppError
 where E: Into<eyre::Report>
 {
     fn from(err: E) -> Self {
-        Self(err.into())
+        Self::Eyre(err.into())
     }
 }
