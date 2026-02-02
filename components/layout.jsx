5c4a31eb4068a2416e476f42c7d6fccfe6014bfc
aspizu <aspizu@protonmail.com>
2026-02-02 20:42:08 +0530

    feat: add HTMX and Hyperscript scripts to layout (#8)

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,6 +5,8 @@ export default function Layout(props) {
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="stylesheet" href="/global.css" />
+                <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+                <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
                 <title>Bunnie Example</title>
             </head>
             <body>{props.children}</body>
